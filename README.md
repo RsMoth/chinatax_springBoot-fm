c44e028a1337de819420d64d9d9a5840f6c77646
RsMoth <liuxie@chinatax.gov.cn>
2019-11-03 00:00:00 +0800

    chinatax_springBoot-fm

---
+++ README.md
@@ -0,0 +1,2 @@
+# chinatax-fm
+基于springBoot财务系统 国家税务总局http://39.99.153.86/
---
+++ src/test/java/com/chinatax/fm/FmApplicationTests.java
@@ -0,0 +1,22 @@
+package com.yuxin.fm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *    国家税务总局
+ *    待上线域名chinatax.gov.cn
+ *    http://39.99.153.86/  admin:chinatax
+ *    liuxie@chinatax.gov.cn
+ * */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
